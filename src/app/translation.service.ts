a9efdca873f8e8b248c22e84a611ffd36a25de5e
Allahverdi <isaevallahverdi@gmail.com>
2025-02-09 01:22:40 +0400

    make language stable

--- src/app/translation.service.ts
+++ src/app/translation.service.ts
@@ -1,21 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TranslationService {
-  constructor(private translate: TranslateService) {
-    const browserLang = translate.getBrowserLang() || 'en';
-    this.translate.setDefaultLang('en');
-    this.translate.use(browserLang);
+  constructor(
+    private translate: TranslateService,
+    @Inject(PLATFORM_ID) private platformId: object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      const savedLang = localStorage.getItem('selectedLanguage') || 'en';
+      this.translate.setDefaultLang('en');
+      this.translate.use(savedLang);
+    } else {
+      this.translate.setDefaultLang('en');
+    }
   }
 
   switchLanguage(lang: string): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('selectedLanguage', lang);
+    }
     this.translate.use(lang);
   }
 
   getCurrentLanguage(): string {
-    return this.translate.currentLang || this.translate.getDefaultLang();
+    return isPlatformBrowser(this.platformId) &&
+      localStorage.getItem('selectedLanguage')
+      ? localStorage.getItem('selectedLanguage')!
+      : 'en';
   }
 }
